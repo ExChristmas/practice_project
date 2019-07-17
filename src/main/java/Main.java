9c227aa8aa781c36070116bb34cff191ba1133c8
ExChristmas <avramenkoav98@mail.ru>
2019-07-18 02:01:55 +0400

    Version 13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.*;
 
 import sortcsv.*;
@@ -5,13 +6,31 @@ import sortcsv.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        List<ReaderCSVSort> readers = new ArrayList<>();
-        readers.add(new DefaultReader("newfile.csv", ',', '"'));
-        readers.add(new DefaultReader("newfile.csv", ',', '"'));
-        WriterCSVSort writer = new DefaultWriter("workfile1.csv",
-                ',', '"', '|', "\n");
-        Comparator<Row> comparator = new DefaultRowComparator("Id");
-        SortCSV sortCSV = new NaturalMergeSort("workfile1.csv", "workfile2.csv");
-        sortCSV.sort("newfile.csv", comparator, readers, writer);
+//        List<ReaderCSVSort> readers = new ArrayList<>();
+//        readers.add(new DefaultReader("newfile.csv", ',', '"'));
+//        readers.add(new DefaultReader("newfile.csv", ',', '"'));
+//        WriterCSVSort writer = new DefaultWriter("workfile1.csv",
+//                ',', '"', '|', "\n");
+//        Comparator<Row> comparator = new DefaultRowComparator("Id");
+//        SortCSV sortCSV = new NaturalMergeSort("workfile1.csv", "workfile2.csv");
+//        sortCSV.sort("newfile.csv", comparator, readers, writer);
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "01234567890";
+        List<String> row = new ArrayList<>();
+        WriterCSVSort writer = new DefaultWriter("bigFile.csv", ',', '"', '|', "\n");
+        row.add("1");
+        row.add("2");
+        row.add("3");
+        row.add("4");
+        row.add("5");
+        row.add("6");
+        row.add("7");
+        writer.write(row);
+        row.clear();
+        for(int i = 0; i < Integer.MAX_VALUE; i++) {
+            for(int j = 0; j < 6; j++) {
+                row.add(alphabet.charAt());
+            }
+        }
     }
 }
